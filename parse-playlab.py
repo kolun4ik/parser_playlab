7d108293edac4435cf42e37c9d2f41e3c610b796
kolun4ik <11_ka@rambler.ru>
2018-09-27 22:43:43 +0300

    осталось повытаскивать карточки

--- parse-playlab.py
+++ parse-playlab.py
@@ -15,23 +15,23 @@ import os
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 from lxml import html
-from pprint import pprint
 
 
 class PlayLabParser:
-    def __init__(self, url='https://playlab.ru/toys/', path='', project='Catalog PlayLab'):
+    def __init__(self, url='https://playlab.ru', path='e:\\', project='Catalog PlayLab'):
         self.url = url
         self.path = path
         self.project = project
         self.page = ''
         self.catalog = []
         self.get_catalog(self.url)
+        self.save_data(self.catalog)
 
 
     def get_html(self, url):
         # получаем по url исходный код страницы на html
         user_agent = UserAgent()
-        self.page = requests.get(self.url, headers={'user-agent': user_agent.chrome})
+        self.page = requests.get(url, headers={'user-agent': user_agent.chrome})
         if self.page.status_code == 200:
             return self.page.text
         else:
@@ -41,8 +41,8 @@ class PlayLabParser:
     def get_catalog(self, url):
         # ищем на странице ul с классом "root" и проходим по всем элементам списка.
         # Помещаем данные в словарь. Формируем список словарей:
-        # [{'name': name, 'url': url, 'image': img, 'categiry': category}]
-        
+        # [{'name': name, 'url': url, 'image': img, 'category': category}]
+        catalog_url = self.url + '/toys/'
         page_code = self.get_html(url)
 
         if page_code is not None:
@@ -61,6 +61,50 @@ class PlayLabParser:
                                      'url': elements_url,
                                      'image': elements_image,
                                      'category': '/'})
+
+    
+    def save_img(self, url, dir=''):
+        image = requests.get('https://playlab.ru' + url)
+        with open(dir+'/%s' % url.split('/')[-1], 'bw') as img:
+            img.write(image.content)
+
+
+    def save_data(self, catalog):
+        if len(catalog) == 0:
+            print("Каталог пустой!")
+            quit()
+        else:
+            print("Заполняем каталог данными!")
+            # Определяем полный путь для каждой категории
+            full_path = os.path.join(self.path, self.project)
+            if os.path.exists(full_path) is False:
+                os.mkdir(full_path)
+            # Пробегаем по списку категорий
+            for element in catalog:
+                current_dir = os.path.join(full_path, element['name'])
+                if os.path.exists(current_dir) is False:
+                    os.makedirs(current_dir)
+                self.save_img(element['image'], current_dir)
+
+                source = str(self.url + element['url'])
+                soup = BeautifulSoup(self.get_html(source), 'lxml')
+                page_keywords = soup.find("meta", {"name":"keywords"})['content']
+                page_desc = soup.find("meta", {"name":"description"})['content']
+                page_title = soup.title.string
+                page_text = soup.find('div', class_='section-description').get_text()
+                pdb.set_trace()
+
+                with open(current_dir + '/page_info.txt', 'w', encoding='utf8') as f_info:
+                    data = {'title': page_title,
+                            'description': page_desc,
+                            'keywords': page_keywords,
+                            'Описание': page_text,
+                            }
+                    for key, value in data.items():
+                        f_info.write(f'{key}: {value}.\n')
+                quit()
+
+
 
 
 
